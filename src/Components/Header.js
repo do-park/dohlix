f61eac0f28e1b7b51a653f81a03827aa6ed90e36
박도희 <dohee.pa@gmail.com>
2021-01-28 23:18:23 +0900

    #3.3 GlobalStyles and Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,19 +2,42 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const List = styled.ul`
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
   display: flex;
-  &:hover {
-    background-color: blue;
-}
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.80);
 `;
 
-const Item = styled.li``;
+const List = styled.ul`
+  display: flex;
+`;
 
-const SLink = styled(Link)``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  margin: auto;
+`;
+
 
 export default () => (
-  <header>
+  <Header>
     <List>
       <Item>
         <SLink to="/">Movies</SLink>
@@ -26,4 +49,4 @@ export default () => (
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
-  </header>)
+  </Header>)
