73fc5e22192f5d6f28301e4a2cd4869c04d01082
박도희 <dohee.pa@gmail.com>
2021-01-30 23:08:02 +0900

    #5.4 Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,8 +3,10 @@ import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom"
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 import Header from "Components/Header"
 
+// eslint-disable-next-line
 export default () => (
   <Router>
     <>
@@ -13,6 +15,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from='*' to='/' />
       </Switch>
     </>
