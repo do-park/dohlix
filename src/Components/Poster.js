6970f4b97dc5b175c6ffd213bb3e8369c418dd14
박도희 <dohee.pa@gmail.com>
2021-02-06 21:58:42 +0900

    #6.7 Poster Component

---
+++ src/Components/Poster.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  font-size: 12px;
+`;
+
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 5px;
+  background-position: center center;
+  transition: opacity 0.1s linear;
+`;
+
+const Rating = styled.span`
+  bottom: 10px;
+  right: 10px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
+
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover{
+    ${Image}{
+      opacity: 0.3;
+    }
+    ${Rating}{
+      opacity: 1;
+    }
+  }
+`;
+
+
+
+const Title = styled.span`
+display: block;
+margin - bottom: 1px;
+`;
+
+const Year = styled.span`
+font - size: 10px;
+color: rgba(255, 255, 255, 0.5);
+`;
+
+const Poster = ({ id, imageUrl, name, title, rating, year, isMovie = false }) => (
+  <Link to={isMovie ? `/ movie / ${id} ` : ` / show / ${id} `}>
+    <Container>
+      <ImageContainer>
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../assets/noPoster.jpg")
+          }
+        />
+        <Rating><span role='img' area-label='rating'>⭐</span>{" "}{rating}/10</Rating>
+      </ImageContainer>
+      <Title>{title.length > 18 ? `${title.substring(0, 15)}...` : title}</Title>
+      <Year>{year}</Year>
+    </Container>
+  </Link>
+);
+
+Poster.propTypes = {
+  id: propTypes.number.isRequired,
+  imageUrl: propTypes.string,
+  title: propTypes.string.isRequired,
+  rating: propTypes.number,
+  year: propTypes.string,
+  isMovie: propTypes.bool,
+
+}
+
+export default Poster;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import Section from 'Components/Section'
 import Loader from 'Components/Loader';
 import Message from 'Components/Message';
+import Poster from 'Components/Poster';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -14,9 +15,51 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
     <Loader />
   ) : (
       <Container >
-        {nowPlaying && nowPlaying.length > 0 && <Section title="Now Playing">{nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-        {upcoming && upcoming.length > 0 && <Section title="Upcoming Movie">{upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-        {popular && popular.length > 0 && <Section title="Popular Movie">{popular.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
+        {nowPlaying && nowPlaying.length > 0 &&
+          <Section title="Now Playing">
+            {nowPlaying.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        }
+        {upcoming && upcoming.length > 0 &&
+          <Section title="Upcoming Movie">
+            {upcoming.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        }
+        {popular && popular.length > 0 &&
+          <Section title="Popular Movie">
+            {popular.map(movie => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        }
         {error && <Message color='#ff4d4d' text={error} />}
       </Container >
     )
